f5bb77360b9bf24a0176e730c073f32f8129b500
VS Charan <138986928+VootlaSaiCharan@users.noreply.github.com>
2023-11-27 09:24:27 +0530

    Create jfPush.groovy
    added jfrog push file

---
+++ vars/jfPush.groovy
@@ -0,0 +1,3 @@
+def cal(){
+  sh 'curl -X PUT -u admin:charan@Akash.98 -T /var/lib/jenkins/workspace/java-3.0/target/kubernetes-configmap-reload-0.0.1-SNAPSHOT.jar "http://35.171.28.175:8082/artifactory/example-repo-local/kubernetes-configmap-reload-0.0.1-SNAPSHOT.jar'
+}
